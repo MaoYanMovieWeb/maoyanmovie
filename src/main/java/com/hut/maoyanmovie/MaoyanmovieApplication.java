1b8a0ee6229a2ecc1bf05ac36c1a0176c3adb365
RoseannePark211 <rljj757858283@163.com>
2020-12-08 19:21:21 +0800

    项目

---
+++ src/main/java/com/hut/maoyanmovie/MaoyanmovieApplication.java
@@ -0,0 +1,13 @@
+package com.hut.maoyanmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaoyanmovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaoyanmovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hut/maoyanmovie/MaoyanmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hut.maoyanmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaoyanmovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
