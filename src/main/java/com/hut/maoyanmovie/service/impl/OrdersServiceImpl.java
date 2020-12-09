01e9bf328248c952ebc4c028f85702faa4c826f2
Cleanmoo <2045222785@qq.com>
2020-12-09 14:18:33 +0800

    创建了Orders控制器  service

---
+++ src/main/java/com/hut/maoyanmovie/service/impl/OrdersServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hut.maoyanmovie.service.impl;
+
+import com.hut.maoyanmovie.service.OrdersService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Cleanmoo
+ * @data 2020/12/9
+ */
+
+@Service
+public class OrdersServiceImpl implements OrdersService {
+}
