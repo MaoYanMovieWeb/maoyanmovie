e6fd72ba225febffd9bea230d25463cb830a0eee
RoseannePark211 <rljj757858283@163.com>
2020-12-09 14:56:45 +0800

    2020年12月9日14:21:37

--- src/test/java/com/hut/maoyanmovie/MaoyanmovieApplicationTests.java
+++ src/test/java/com/hut/maoyanmovie/MaoyanmovieApplicationTests.java
@@ -1,13 +1,20 @@
 package com.hut.maoyanmovie;
 
+import com.hut.maoyanmovie.bean.Movie;
+import com.hut.maoyanmovie.dao.MovieMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MaoyanmovieApplicationTests {
 
+    @Autowired
+    MovieMapper movieMapper;
+
     @Test
     void contextLoads() {
+        System.out.println(movieMapper.getMovieByName("湄公河"));
     }
 
 }
