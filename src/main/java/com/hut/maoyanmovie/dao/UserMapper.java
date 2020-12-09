35c5b5d9c5299be5aabe77f145e959dec584324b
345404748 <345404748@qq.com>
2020-12-09 14:44:45 +0800

    2020年12月8日 20:54:16 djh

---
+++ src/main/java/com/hut/maoyanmovie/dao/UserMapper.java
@@ -0,0 +1,13 @@
+package com.hut.maoyanmovie.dao;
+
+import com.hut.maoyanmovie.bean.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author HP
+ * @data 2020-12-09
+ */
+@Mapper
+public interface UserMapper {
+
+}
