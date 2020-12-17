7f72f2d0995c92b1f56b100c2776bf6cb0883a55
345404748 <345404748@qq.com>
2020-12-17 15:38:42 +0800

    2020年12月17日 15:38:36

--- src/test/java/com/hut/maoyanmovie/MaoyanmovieApplicationTests.java
+++ src/test/java/com/hut/maoyanmovie/MaoyanmovieApplicationTests.java
@@ -1,10 +1,8 @@
 package com.hut.maoyanmovie;
 
-import com.hut.maoyanmovie.dao.UserMapper;
-import com.hut.maoyanmovie.service.MovieService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class MaoyanmovieApplicationTests {
@@ -13,5 +11,4 @@ class MaoyanmovieApplicationTests {
     void contextLoads() {
 
     }
-
 }
